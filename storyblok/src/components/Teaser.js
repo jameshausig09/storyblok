01fd99c7c77231c128f4e5735146d9aa5cfbf40f
James Seidel <james.seidel@2128-072021.localdomain>
2024-07-05 16:33:47 +0200

    styling the Teaser.js

--- storyblok/src/components/Teaser.js
+++ storyblok/src/components/Teaser.js
@@ -1,12 +1,15 @@
+import "./Teaser.css";
 import { storyblokEditable } from "@storyblok/react";
 
 const Teaser = ({ blok }) => {
+  console.log(blok);
   return (
-    <div {...storyblokEditable(blok)}>
+    <div {...storyblokEditable(blok)} className="Teaser">
       <h2 className="py-32 text-6xl text-[#50b0ae] font-bold text-center">
         {blok.headline}
       </h2>
       <p>{blok.description}</p>
+      {blok.Image && <img src={blok.Image.filename} alt={blok.Image.alt} />}
     </div>
   );
 };
