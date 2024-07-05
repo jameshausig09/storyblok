01929f9246af9f8a719f0ef2690c07dea6eedd6e
hugh.burgess@db-n.com <hugh.burgess@db-n.com>
2024-07-05 16:43:17 +0200

    teaser note

--- storyblok/src/components/Teaser.js
+++ storyblok/src/components/Teaser.js
@@ -2,7 +2,7 @@ import "./Teaser.css";
 import { storyblokEditable } from "@storyblok/react";
 
 const Teaser = ({ blok }) => {
-  console.log(blok);
+  console.log(blok); // if you are not using this, you can remove this whole line - (good coding practice to remove unused code or console.log statements)
   return (
     <div {...storyblokEditable(blok)} className="Teaser">
       <h2 className="py-32 text-6xl text-[#50b0ae] font-bold text-center">
