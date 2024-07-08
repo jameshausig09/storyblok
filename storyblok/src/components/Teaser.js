325adedd714062f3ca7ea9ca17d8115be0644b05
James Seidel <james.seidel@2128-072021.localdomain>
2024-07-08 16:35:42 +0200

    adding guards to teaser.js

--- storyblok/src/components/Teaser.js
+++ storyblok/src/components/Teaser.js
@@ -2,13 +2,21 @@ import "./Teaser.css";
 import { storyblokEditable } from "@storyblok/react";
 
 const Teaser = ({ blok }) => {
+  if (!blok) {
+    return null;
+  }
+
   return (
     <div {...storyblokEditable(blok)} className="Teaser">
-      <h2 className="py-32 text-6xl text-[#50b0ae] font-bold text-center">
-        {blok.headline}
-      </h2>
-      <p>{blok.description}</p>
-      {blok.Image && <img src={blok.Image.filename} alt={blok.Image.alt} />}
+      {blok.headline && (
+        <h2 className="py-32 text-6xl text-[#50b0ae] font-bold text-center">
+          {blok.headline.replace("+", " ")}
+        </h2>
+      )}
+      {blok.description && <p>{blok.description}</p>}
+      {blok.Image?.filename && (
+        <img src={blok.Image.filename} alt={blok.Image.alt || "Teaser"} />
+      )}
     </div>
   );
 };
