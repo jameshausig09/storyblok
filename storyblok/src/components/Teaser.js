9128dbbbd0e572465a73dc4e6ab46eadb28341f9
James Seidel <james.seidel@2128-072021.db-n.com>
2024-07-09 13:39:26 +0200

    fetch data - in progress

--- storyblok/src/components/Teaser.js
+++ storyblok/src/components/Teaser.js
@@ -1,7 +1,29 @@
+import React from "react";
+// { useEffect, useState }
 import "./Teaser.css";
 import { storyblokEditable } from "@storyblok/react";
 
 const Teaser = ({ blok }) => {
+  //   const [weatherData, setWeatherData] =useState(null);
+
+  //   useEffect(() => {
+  // const fetchWeatherData = async axios.get(
+  //   "https://api.openweathermap.org/data/2.5/weather",
+  //   {
+  //     params: {
+  //       q: "Paris",
+  //       appid: "c6f9c5276d42f0a32ce26ae3d1334051",
+  //       units: "metric,"
+  //     },
+  //   }
+  // );
+  // setWeatherData(response.data);
+  //   } catch (error) {
+  //     console.error("Error fetching weather data:", error);
+  //   }
+  // };
+
+  //   }
   if (!blok) {
     return null;
   }
